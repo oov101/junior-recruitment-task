66cdff0b91ed62050739201aef4ce40692d3dc1a
Dawid Walenciej <dawid101w@gmail.com>
2018-09-29 00:32:31 +0200

    server POST, DELETE methods added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 const express = require('express')
-const app = express()
 var mongoose = require('mongoose');
+const app = express()
 const port = 3000
 
 // CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+app.use(express.json())
+app.use('/to-do-list/frontend', express.static('frontend'))
 
 // Conecting to database
 mongoose.connect('mongodb://user:user123@ds215563.mlab.com:15563/qunabu-junior-recruitment-task', { useNewUrlParser: true });
@@ -26,8 +29,6 @@ var taskSchema = new mongoose.Schema({
 
 var Task = mongoose.model('Task', taskSchema);
 
-app.use('/to-do-list/frontend', express.static('frontend'))
-
 app.get('/to-do-list/backend/tasks', function (req, res, next) {
   Task.find().lean().exec(function (err, tasks) {
     if (err) return console.error(err);
@@ -37,6 +38,27 @@ app.get('/to-do-list/backend/tasks', function (req, res, next) {
 
 app.get('/to-do-list/backend', function (req, re, next) {
   res.send('Server Running!')
+})
+
+app.post('/to-do-list/backend/new_task', (req, res, next) => {
+  console.log(req.body)
+  const newTask = new Task(req.body);
+    newTask.save(err => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(newTask);
+});
+})
+
+app.delete('/to-do-list/backend/:task_id', (req, res, next) => {
+  console.log(req.params.task_id);
+  Task.findByIdAndRemove(req.params.task_id, (err, task) => {
+    if (err) return res.status(500).send(err);
+    const response = {
+        message: "Todo successfully deleted",
+        id: task._id
+    };
+    return res.status(200).send(response);
+  });
 })
 
 
