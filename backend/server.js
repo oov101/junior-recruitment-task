6286933ff674d930c3060405d91b0e16b3b432d4
oOv <dawid101w@gmail.com>
2018-09-29 01:22:37 +0200

    server PUT method added

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 // CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
@@ -15,10 +15,9 @@ app.use('/to-do-list/frontend', express.static('frontend'))
 
 // Conecting to database
 mongoose.connect('mongodb://user:user123@ds215563.mlab.com:15563/qunabu-junior-recruitment-task', { useNewUrlParser: true });
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
   // we're connected!
 });
 
@@ -43,10 +42,24 @@ app.get('/to-do-list/backend', function (req, re, next) {
 app.post('/to-do-list/backend/new_task', (req, res, next) => {
   console.log(req.body)
   const newTask = new Task(req.body);
-    newTask.save(err => {
+  newTask.save(err => {
     if (err) return res.status(500).send(err);
     return res.status(200).send(newTask);
-});
+  });
+})
+
+app.put('/to-do-list/backend/:task_id', (req, res, next) => {
+  console.log(req.params.task_id, req.body);
+
+  Task.findById(req.params.task_id, function (err, task) {
+    if (err) return handleError(err);
+
+    task.set(req.body);
+    task.save(function (err, updatedTask) {
+      if (err) return handleError(err);
+      res.send(updatedTask);
+    });
+  });
 })
 
 app.delete('/to-do-list/backend/:task_id', (req, res, next) => {
@@ -54,8 +67,8 @@ app.delete('/to-do-list/backend/:task_id', (req, res, next) => {
   Task.findByIdAndRemove(req.params.task_id, (err, task) => {
     if (err) return res.status(500).send(err);
     const response = {
-        message: "Todo successfully deleted",
-        id: task._id
+      message: "Todo successfully deleted",
+      id: task._id
     };
     return res.status(200).send(response);
   });
