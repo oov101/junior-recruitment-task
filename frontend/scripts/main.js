3293670b0d968fa4ad87dd0480b3a4793e10d455
oOv <dawid101w@gmail.com>
2018-09-28 03:48:05 +0200

    Render tasks

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,6 +1,60 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+  new ToDoApp();
+});
+
 /**
- * This function tests whether the JS is running and documentationjs is working corectly
+ * This is To-Do List Application
  */
-function isJavascriptAndDocumentationWorking() {
-  console.log('JS is working!');
+class ToDoApp {
+  constructor() {
+    this.appNode = document.getElementsByClassName('app')[0];
+    this.getTasks();
+  }
+
+  /**
+   * Get tasks from server as object from JSON
+   */
+  getTasks() {
+    fetch('http://localhost:3000/to-do-list/backend/tasks')
+      .then(response => response.json())
+      .then(tasks => {
+        this.renderTasks(tasks);
+      });
+  }
+
+  /**
+   * Render tasks to app container
+   * @param {Array} tasks 
+   */
+  renderTasks(tasks) {
+    tasks.forEach(task => {
+      this.appNode.appendChild(this.createTaskNode(task._id, task.isDone, task.contents));
+    });
+  }
+
+  /**
+   * Creating Node for task. Adding proper id, classes and contents for single task.
+   * @param {Number} id 
+   * @param {Boolean} isDone 
+   * @param {String} contents
+   * @return {Element}
+   */
+  createTaskNode(id, isDone, contents) {
+    const taskNode = document.createElement('div');
+    let taskStatusClass = 'to-do';
+
+    if (isDone) {
+      taskStatusClass = 'done';
+    }
+
+    taskNode.innerHTML = `
+      <div id=${id} class="task ${taskStatusClass}">
+        <div class="checker"></div>
+        <div class="content">${contents}</div>
+        <div class="trash"></div>
+      </div>
+    `;
+
+    return taskNode;
+  }
 }
