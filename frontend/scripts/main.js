7c3ed709f3a0a30204e67349ff73e8937cf028a6
oOv <dawid101w@gmail.com>
2018-09-29 20:34:44 +0200

    front-end JS refactor

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -4,75 +4,77 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 /**
  * This is To-Do List Application
- * @param {String} title
  */
 class ToDoApp {
-  constructor(title) {
-    this.appNode = document.getElementsByClassName('app')[0];
-    this.createTitleBar(title);
-    this.tasksContainer = this.createTaskContainer();
-    this.createInputBar();
+  constructor() {
+    this.initElements();
     this.getTasks();
   }
 
   /**
-   * Creates tasks container for all tasks
-   * @return {Element}
+   * Initialize necessary DOM Elements
    */
-  createTaskContainer() {
-    const tasksContainer = document.createElement('div');
-    tasksContainer.setAttribute('class', 'tasks-container');
-    this.appNode.appendChild(tasksContainer);
-    return tasksContainer;
+  initElements() {
+    this.appNode = document.getElementsByClassName('app')[0];
+    this.titleBarTemplate();
+    this.tasksContainer = document.createElement('div');
+    this.inputBarNode = document.createElement('div');
+    this.addTaskNode = document.createElement('div');
+    this.taskBarSeparatorNode = document.createElement('div');
+    this.inputNode = document.createElement('input');
+
+    this.tasksContainer.setAttribute('class', 'tasks-container');
+    this.inputBarNode.classList = 'input-bar task';
+    this.addTaskNode.classList = 'add-task';
+    this.taskBarSeparatorNode.classList = 'task-bar-separator';
+
+    this.appNode.appendChild(this.tasksContainer);
+    this.inputBarNode.append(this.addTaskNode, this.taskBarSeparatorNode, this.inputNode);
+    this.appNode.appendChild(this.inputBarNode);
+
+    this.addTaskNode.addEventListener('click', () => this.createNewTask());
   }
 
   /**
-   * Create title bar for to-do list
-   * @param {String} title 
+   * HTML Template of title bar
    */
-  createTitleBar(title) {
+  titleBarTemplate() {
     this.appNode.innerHTML = `
       <div class="title-bar">
         <div class="title-bar-separator"></div>
-        <p>${title}</p>
+        <p>ToDo List</p>
       </div>
     `;
   }
 
   /**
-   * Create input bar for task adding
+   * CRUDTemplate with view rerender
+   * @param {String} link 
+   * @param {Object} data 
+   * @param {String} method 
    */
-  createInputBar() {
-    const inputBarNode = document.createElement('div');
-    const addTaskNode = document.createElement('div');
-    const taskBarSeparatorNode = document.createElement('div');
-    const inputNode = document.createElement('input');
+  CRUDTemplate(link, data, method) {
+    fetch(`http://localhost:3000/to-do-list/backend/${link}`, {
+      method: `${method}`,
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+      .then(response => console.log('Success:', JSON.stringify(response)))
+      .catch(error => console.error('Error:', error))
+      .then(() => this.getTasks());
+  }
 
-    inputBarNode.classList = 'input-bar task';
-    addTaskNode.classList = 'add-task';
-    taskBarSeparatorNode.classList = 'task-bar-separator';
-
-    inputBarNode.append(addTaskNode, taskBarSeparatorNode, inputNode);
-
-    addTaskNode.addEventListener('click', () => {
-      if (inputNode.value.length === 0) return;
-      const data = { isDone: false, contents: inputNode.value };
-
-      fetch('http://localhost:3000/to-do-list/backend/new_task', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json())
-        .then(response => console.log('Success:', JSON.stringify(response)))
-        .catch(error => console.error('Error:', error))
-        .then(() => this.getTasks());
-
-      inputNode.value = "";
+  /**
+   * Render tasks to task container
+   * @param {Array} tasks 
+   */
+  renderTasks(tasks) {
+    this.tasksContainer.innerHTML = "";
+    tasks.forEach(task => {
+      this.tasksContainer.appendChild(this.createTaskNode(task._id, task.isDone, task.contents));
     });
-
-    this.appNode.appendChild(inputBarNode);
   }
 
   /**
@@ -87,18 +89,47 @@ class ToDoApp {
   }
 
   /**
-   * Render tasks to tasks container
-   * @param {Array} tasks 
-   */
-  renderTasks(tasks) {
-    this.tasksContainer.innerHTML = "";
-    tasks.forEach(task => {
-      this.tasksContainer.appendChild(this.createTaskNode(task._id, task.isDone, task.contents));
-    });
+  * Validates and creates new task, clears input field after all
+  */
+  createNewTask() {
+    if (this.inputNode.value.length === 0) return;
+    const newTask = { isDone: false, contents: this.inputNode.value };
+    this.CRUDTemplate('new_task', newTask, 'POST');
+    this.inputNode.value = "";
   }
 
   /**
-   * Creating Node for task. Adding proper id, classes and contents for single task.
+   * Delete task of id
+   * @param {Number} id 
+   */
+  deleteTask(id) {
+    this.CRUDTemplate(id, undefined, 'DELETE');
+  }
+
+  /**
+   * Toggle Task between done and todo
+   * @param {Number} id 
+   * @param {Boolean} isDone 
+   */
+  toggleTask(id, isDone) {
+    this.CRUDTemplate(id, { isDone: !isDone }, 'PUT');
+  }
+
+  /**
+   * Validates and allows to edit tasks
+   * @param {Number} id 
+   * @param {String} newContents 
+   */
+  editTask(id, newContents) {
+    if (newContents.length === 0) {
+      this.getTasks();
+      return;
+    };
+    this.CRUDTemplate(id, { contents: newContents }, 'PUT');
+  }
+
+  /**
+   * Creating a task. Adding proper id, classes and contents for single task.
    * @param {Number} id 
    * @param {Boolean} isDone 
    * @param {String} contents
@@ -107,73 +138,59 @@ class ToDoApp {
   createTaskNode(id, isDone, contents) {
     let taskStatusClass = '';
 
+    /**
+     * Initialize taskbar elements 
+     */
+    const initTaskElements = () => {
+      this.taskNode = document.createElement('div');
+      this.checkerNode = document.createElement('div');
+      this.taskBarSeparatorNode = document.createElement('div');
+      this.taskContentsContainerNode = document.createElement('div');
+      this.taskContentsNode = document.createElement('p');
+      this.trashNode = document.createElement('div');
+    };
+
+    /**
+     * Append Elements to proper parents
+     */
+    const moveTaskElementsDOM = () => {
+      this.taskNode.append(this.checkerNode, this.taskBarSeparatorNode, this.taskContentsContainerNode, this.trashNode);
+      this.taskContentsContainerNode.appendChild(this.taskContentsNode);
+    }
+
+    /**
+     * Setting id's and classes to elements
+     */
+    const setAttributesToTaskElements = () => {
+      this.taskNode.setAttribute('id', `${id}`);
+      this.taskContentsNode.setAttribute('contenteditable', 'true');
+      this.taskNode.className = `task ${taskStatusClass}`;
+      this.checkerNode.className = 'checker';
+      this.taskBarSeparatorNode.className = 'task-bar-separator';
+      this.taskContentsContainerNode.className = 'content';
+      this.trashNode.className = 'trash';
+    }
+
+    /**
+     * Adding event listeners for toggling, editing, deleting tasks
+     */
+    const addEventListenersToTaskElements = () => {
+      this.taskContentsNode.addEventListener('focusout', () => this.editTask(id, this.taskContentsNode.innerText));
+      this.checkerNode.addEventListener('click', () => this.toggleTask(id, isDone));
+      this.trashNode.addEventListener('click', () => this.deleteTask(id));
+    };
+
     if (isDone) {
       taskStatusClass = 'done';
     }
 
-    const taskNode = document.createElement('div');
-    const checkerNode = document.createElement('div');
-    const taskBarSeparatorNode = document.createElement('div');
-    const taskContentsContainerNode = document.createElement('div');
-    const taskContentsNode = document.createElement('p');
-    const trashNode = document.createElement('div');
+    initTaskElements();
+    moveTaskElementsDOM();
+    setAttributesToTaskElements();
+    addEventListenersToTaskElements();
 
-    taskNode.setAttribute('id', `${id}`);
-    taskContentsNode.setAttribute('contenteditable', 'true');
-    taskNode.className = `task ${taskStatusClass}`;
-    checkerNode.className = 'checker';
-    taskBarSeparatorNode.className = 'task-bar-separator';
-    taskContentsContainerNode.className = 'content';
-    trashNode.className = 'trash';
-    taskContentsNode.innerHTML = contents;
+    this.taskContentsNode.innerHTML = contents;
 
-    taskNode.append(checkerNode, taskBarSeparatorNode, taskContentsContainerNode, trashNode);
-    taskContentsContainerNode.appendChild(taskContentsNode);
-
-    taskContentsNode.addEventListener('focusout', () => {
-      if (taskContentsNode.innerText.length === 0) {
-        this.getTasks();
-        return;
-      };
-
-      const data = {contents: taskContentsNode.innerText};
-
-      fetch(`http://localhost:3000/to-do-list/backend/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json())
-        .then(response => console.log('Success:', JSON.stringify(response)))
-        .catch(error => console.error('Error:', error))
-        .then(() => this.getTasks());
-    });
-
-    checkerNode.addEventListener('click', () => {
-      const data = { isDone: !isDone };
-
-      fetch(`http://localhost:3000/to-do-list/backend/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json())
-        .then(response => console.log('Success:', JSON.stringify(response)))
-        .catch(error => console.error('Error:', error))
-        .then(() => this.getTasks());
-    });
-
-    trashNode.addEventListener('click', () => {
-      fetch(`http://localhost:3000/to-do-list/backend/${id}`, {
-        method: 'DELETE',
-      }).then(res => res.json())
-        .then(response => console.log('Success:', JSON.stringify(response)))
-        .catch(error => console.error('Error:', error))
-        .then(() => this.getTasks());
-    });
-
-    return taskNode;
+    return this.taskNode;
   }
 }
